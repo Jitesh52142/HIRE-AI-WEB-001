3ba6c2175ceaa963098d8c66e345cc34355eb6d3
jitesh bawaskar <jiteshbawaskar05@gmail.com>
2025-08-25 20:51:25 +0530

    final commit

--- api/index.py
+++ api/index.py
@@ -1,8 +1,9 @@
+# api/index.py
 from app import create_app
+from serverless_wsgi import handle_request  # Required for Vercel
 
 app = create_app()
 
-# Vercel looks for a variable named "app"
-# So we expose it here
-
-
+# Vercel expects a handler function
+def handler(event, context):
+    return handle_request(app, event, context)
