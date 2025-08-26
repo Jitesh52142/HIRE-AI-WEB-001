f9e0657044ad9364aae08bbb50843d902530e8ec
jitesh bawaskar <jiteshbawaskar05@gmail.com>
2025-08-26 13:17:28 +0530

    final

--- api/index.py
+++
@@ -1,9 +0,0 @@
-# api/index.py
-from app import create_app
-from serverless_wsgi import handle_request  # Required for Vercel
-
-app = create_app()
-
-# Vercel expects a handler function
-def handler(event, context):
-    return handle_request(app, event, context)
